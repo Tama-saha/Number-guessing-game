02d75174b01c07fbd554a88b8921a95884f337f8
Tama saha <73534942+Tama-saha@users.noreply.github.com>
2021-01-26 11:50:55 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+let low = 1;
+
+let high = 10;
+
+for(i =1;i<=3;i++){
+
+    let gusessNumber=parseInt(prompt("Enter the number from 1 to 10"));
+
+    let correct_ans= Math.floor(Math.random()*10) + 1;
+    
+    if(gusessNumber < correct_ans){
+
+       alert("Correct answer is greater!")
+
+    }else if(gusessNumber > correct_ans){
+
+        alert("Correct answer is smaller!")
+    }
+    else if(gusessNumber = correct_ans){
+        
+        alert("You win");
+    }
+        else{
+            alert("You lose!");
+        }
+}
 
